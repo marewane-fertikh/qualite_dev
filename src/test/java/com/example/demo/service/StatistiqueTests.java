80760c7d3257ea0f1d8be885ba0dd41447ad81fd
marewane-fertikh <marewane.fertikh@edu.univ-paris13.fr>
2025-05-21 11:30:57 +0200

    modification de la classe StatistiqueTests.java dans la branche newcarservice

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -14,4 +15,46 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    public void testAjouterVoiture() {
+        // Création d'une voiture de test
+        Voiture voiture = new Voiture("Renault", 20000);
+
+        // Test de la méthode ajouter
+        statistiqueImpl.ajouter(voiture);
+
+        // Vérification que la méthode a été appelée avec la bonne voiture
+        verify(statistiqueImpl, times(1)).ajouter(voiture);
+    }
+
+    @Test
+    public void testPrixMoyen() {
+        // Création d'un échantillon attendu
+        Echantillon echantillonAttendu = new Echantillon(2, 25000);
+
+        // Configuration du mock
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillonAttendu);
+
+        // Appel de la méthode
+        Echantillon resultat = statistiqueImpl.prixMoyen();
+
+        // Vérifications
+        assertNotNull(resultat);
+        assertEquals(2, resultat.getNombreDeVoitures());
+        assertEquals(25000, resultat.getPrixMoyen());
+        verify(statistiqueImpl, times(1)).prixMoyen();
+
+    }
+
+    @Test void testPrisMoyenListeVide() {
+        // Configuration du mock pour simuler une liste vide
+        when(statistiqueImpl.prixMoyen()).thenThrow(new ArithmeticException());
+
+        // Vérification que l'exception est bien levée
+        assertThrows(ArithmeticException.class, () -> statistiqueImpl.prixMoyen());
+    }
+
 }
+
+
+
