1ff5b1a1960366763bd632f9ecdc9f73ddd51ffe
marewane-fertikh <marewane.fertikh@edu.univ-paris13.fr>
2025-05-21 10:20:49 +0200

    newcarservice

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -11,7 +11,17 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+
+
+        // Création d'une voiture avec le constructeur avec paramètres
+        Voiture voiture = new Voiture("Renault", 20000);
+
+        // Vérification des valeurs initiales
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(20000, voiture.getPrix());
+        assertEquals(0, voiture.getId());
     }
 
 }
+
+
